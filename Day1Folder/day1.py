d4d2e646419ec16f3768e8f27c9fdb5a643c5abb
harveymtjiam-jpg <harveymtjiam@gmail.com>
2026-06-23 03:52:31 +0000

    Jun 23, 2026, 3:52 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,8 +4,12 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
+print("My Neck Hurts")
 
-
+print("My name is Harvey")
+print("I am 13 years old")
+print("I attend ACS Jakarta")
+print("Denzel")
 
 ########################################################################
 # Task 2:
@@ -14,26 +18,42 @@ print("hello from day1")
 
 ########################################################################
 # Task 3:
-
-
-
+animal1="Monkey"
+animal2="Gorilla"
+animal3="Denzel Wilson Chandra"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
+variable1= 2
+variable2= 5
+print(variable1 + variable2)
 
 ########################################################################
 # Task 5:
+name = "Rat"
+food = "Garbage"
+print("Denzel is a "+ name)
+print("Denzel eats "+ food)
 
-
-
+print("The animal is a " + name)
 ########################################################################
 # Task 6:
+def intro():
+    print("My name is Harvey")
+    print("I am 13 years old")
+    print("I attend ACS Jakarta")
+    print("I would buy games through with $2000")
 
-
+intro()
 
 ########################################################################
 # Task 7:
+def addition(number1, number2):
+    print(number1 + number2)
+    
+addition(12, 342)
 
 
 
--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -3,34 +3,64 @@
 print("hello from day2")
 
 ########################################################################
-# Task 1:
+# Task 1
 
-
-
+for count in range(5):
+    print("Neck Hurts")
 ########################################################################
 # Task 2:
 
+name="Apu"
+
+for potato in name:
+    print("Give me a "+potato)
+print(name)
 
 
 ########################################################################
 # Task 3:
-
+for counter in range(9):
+    print(counter)
 
 
 ########################################################################
 # Task 4:
-
+for count in range(1,11):
+    print(count)
 
 
 ########################################################################
 # Task 5:
-
+for count in range(2,101,2):
+    print(count)
 
 
 ########################################################################
 # Task 6:
-
+for count in range(100,0,-1):
+    print(count)
 
 
 ########################################################################
 # Additional exercises:
+for count in range(0,67):
+    print(count)
+    
+for count in range(0,101):
+    print(count)
+
+for count in range(7,33):
+    print(count)
+
+for count in range(65,100):
+    print(count)
+
+for count in range(2,33,2):
+    print(count)
+
+for count in range(5,101,5):
+    print(count)
+
+for count in range(100,0,-1):
+    print(count)
+
