a1ce24580706a4cd62df47327175d02b46a6351f
harveymtjiam-jpg <harveymtjiam@gmail.com>
2026-06-23 04:07:37 +0000

    Jun 23, 2026, 4:07 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,8 +5,8 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 Myname = input ("What is your name")
-Myname = input ()
-
+Myname = input ("What is your name")
+Myname = input("")
 ########################################################################
 # Task 2:
 
