348029f341df71df75fd9c9220cc5fcafab60f44
harveymtjiam-jpg <harveymtjiam@gmail.com>
2026-06-23 03:55:36 +0000

    Jun 23, 2026, 3:55 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -64,3 +64,40 @@ for count in range(5,101,5):
 for count in range(100,0,-1):
     print(count)
 
+def Teleport():
+    agent.teleport_to_player()
+
+player.on_chat("come", Teleport)
+
+
+def move_forward(steps):
+    agent.move(FORWARD, steps)
+player.on_chat("forward", move_forward)
+def move_back(steps):
+    agent.move(BACK,steps)
+player.on_chat("back", move_back)
+def move_up(steps):
+    agent.move(UP,steps)
+player.on_chat("up", move_up)
+def move_down(steps):
+    agent.move(DOWN,steps)
+player.on_chat("down", move_down)
+def turn_left():
+    agent.turn(LEFT)
+player.on_chat("left", turn_left)
+def turn_right():
+    agent.turn(RIGHT)
+player.on_chat("right", turn_right)
+
+def bomb():
+    agent.set_item(TNT, 64, 1)
+    agent.place(FORWARD)
+player.on_chat("place_tnt", bomb)
+
+def chop_wood(mine):
+    for destroy_wood in range(mine):
+        agent.destroy(FORWARD)
+        agent.move(FORWARD,1)
+    agent.move(BACK,mine)
+    agent.collect_all()
+player.on_chat("destroy", chop_wood)
