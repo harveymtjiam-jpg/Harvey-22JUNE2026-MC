4387383e3a8969ca364a7d242a8b13435c77fdd0
harveymtjiam-jpg <harveymtjiam@gmail.com>
2026-06-24 03:06:36 +0000

    Jun 24, 2026, 3:06 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -57,8 +57,7 @@ for count in range (10):
 import random
 num1 = random.randint(1,100)
 num2 = random.randint(1,100)
-if num1 + num2:
-    print
+
 
 
 ########################################################################
