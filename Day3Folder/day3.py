adae7ffd10a469301c7d3d4ba21165288320c12d
harveymtjiam-jpg <harveymtjiam@gmail.com>
2026-06-24 03:04:24 +0000

    Jun 24, 2026, 3:04 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -56,7 +56,7 @@ for count in range (10):
 # Task 8:
 import random
 diceValue = random.randint(1,100)
-diceValue1
+diceValue1 = ranodm.randint(1,100)
 
 
 ########################################################################
